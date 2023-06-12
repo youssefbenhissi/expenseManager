7e20e3c94616a7c9ac1995fe759c817bf5c75a2b
DESKTOP-040L16N\FE <mohamed-ali.nammouchi@quodfinancial.com>
2023-06-12 11:18:14 +0200

    fingerprint authentication added

--- android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
@@ -2,5 +2,5 @@ package com.example.expense_manager
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
