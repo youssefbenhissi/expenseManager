b6f4084150bea2d0914cc45aef170dc217ac9846
DESKTOP-040L16N\FE <mohamed-ali.nammouchi@quodfinancial.com>
2023-06-17 13:36:28 +0200

    fix the configuration of fingerprint

--- android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/expense_manager/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.expense_manager
 
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.embedding.android.FlutterFragmentActivity
 class MainActivity: FlutterFragmentActivity() {
 }
